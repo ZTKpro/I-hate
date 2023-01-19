c75b0b8f6a40dcd044f0305c5568178aed775e78
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2023-01-20 00:17:31 +0100

    new page - conifgurator 3D

--- components/data/menuItems.js
+++ components/data/menuItems.js
@@ -1,58 +1,47 @@
 const MenuItems = [
   {
-    id: 1,
     title: "AI",
     items: [
       {
-        id: 1,
         title: "Detector",
         url: "/ai/detector",
       },
       {
-        id: 2,
         title: "Car neural network",
         url: "https://car-network.netlify.app",
       },
       {
-        id: 3,
         title: "Classifier",
         url: "https://classifier-js-ds.netlify.app",
       },
     ],
   },
   {
-    id: 2,
     title: "Blockchain",
     items: [
       {
-        id: 1,
         title: "Wallet",
         url: "https://github.com/ZTKpro/BlockChain",
       },
     ],
   },
   {
-    id: 2,
     title: "Style",
     items: [
       {
-        id: 1,
-        title: "3D show",
-        url: "/",
+        title: "3D configurator",
+        url: "/style/configurator",
       },
       {
-        id: 2,
         title: "Parallax",
         url: "/style/parallax",
       },
     ],
   },
   {
-    id: 2,
     title: "Another",
     items: [
       {
-        id: 1,
         title: "Motion Detection",
         url: "https://motion-detection-ds.netlify.app",
       },
---
+++ pages/style/configurator.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+import styled from "styled-components";
+
+import Content from "../../components/content";
+
+export default function Configurator() {
+  return (
+    <Content padding="0 0 0 10px">
+      <h1>Configurator</h1>
+    </Content>
+  );
+}
