4d93dff1aca136387c7000a94940ca8d75ca4bb7
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2023-01-20 01:21:53 +0100

    add valtio and init state

--- pages/style/configurator.js
+++ pages/style/configurator.js
@@ -1,8 +1,27 @@
-import { useState, useEffect } from "react";
+import React, { Suspense, useRef, useState, useEffect } from "react";
+import { ContactShadows, Environment, useGLTF, OrbitControls } from "drei";
+import { proxy, useProxy } from "valtio";
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
 import Content from "../../components/content";
+
+const state = proxy({
+  current: null,
+  items: {
+    laces: "#ffffff",
+    mesh: "#ffffff",
+    caps: "#ffffff",
+    inner: "#ffffff",
+    sole: "#ffffff",
+    stripes: "#ffffff",
+    band: "#ffffff",
+    patch: "#ffffff",
+  },
+});
+
+
+function Shoe() {
 
 export default function Configurator() {
   return (
