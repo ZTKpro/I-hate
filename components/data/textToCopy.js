20b69a16a0d9c4fa9214e48f7b9cfa79e372bc67
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2023-01-26 07:54:35 +0100

    copier

---
+++ components/data/textToCopy.js
@@ -0,0 +1,51 @@
+export const en = `
+Hello!
+
+If you are looking for an experienced front-end developer, that's me!
+
+I have a wide range of knowledge and skills in front-end development, as well as valuable work experience.
+
+I am a responsible, reliable, and conscientious person, and I learn new things quickly. I enjoy working in teams and can adapt to different situations.
+
+I believe that I can be a valuable member of your team and contribute to achieving common goals.
+
+I encourage you to check out my recent projects on my website:
+https://ds-code.netlify.app
+
+I would be happy to answer any questions during the interview and present my qualifications and skills in front-end development.
+
+I hope you will consider hiring me and I can join your team!
+
+Best regards,
+Damian Szmurło
+`;
+
+export const pl = `
+Jeśli szukasz doświadczonego programisty front-end, to właśnie ja!
+
+Posiadam szerokie spektrum wiedzy i umiejętności z zakresu programowania front-end oraz cenne doświadczenie zawodowe.
+
+Jestem osobą odpowiedzialną, rzetelną i sumienną, a także szybko uczę się nowych rzeczy.
+Lubię pracować w zespołach i potrafię dostosować się do różnych sytuacji.
+
+Wierzę, że mogę być wartościowym członkiem Twojego zespołu i przyczynić się do osiągnięcia wspólnych celów.
+
+Zachęcam do zapoznania się z moimi profilami na LinkedIn i GitHub:
+- linkedin.com/in/dszmurlo
+- github.com/ZTKpro
+
+oraz z moimi ostatnimi projektami na stronie internetowej: 
+https://ds-code.netlify.app
+
+Chętnie odpowiem na wszelkie pytania podczas rozmowy kwalifikacyjnej i przedstawię swoje kwalifikacje
+ i umiejętności.
+
+Mam nadzieję, że zdecydujesz się na zatrudnienie mnie i mogę dołączyć do Twojego zespołu!
+
+Pozdrawiam,
+Damian Szmurło
+`;
+
+export const link = `linkedin.com/in/dszmurlo/`;
+
+export const git = `github.com/ZTKpro`;
--- pages/for-me/cheat-sheet.js
+++ pages/for-me/cheat-sheet.js
@@ -1,22 +1,53 @@
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 import styled from "styled-components";
 
 import Content from "../../components/content";
 import Tile from "../../components/tile";
-import Bubble from "../../components/bubble";
+import { en, pl, link, git } from "../../components/data/textToCopy";
 
 const StyledRow = styled.div`
   display: flex;
 `;
+
 const StyledH2 = styled.h2`
   margin: 20px 10px;
   font-size: 32px;
 `;
 
+const StyledCopy = styled.p`
+  cursor: pointer;
+  &:hover {
+    color: #1aaffc;
+  }
+`;
+
+const StyledInfo = styled.div`
+  z-index: 10;
+  position: absolute;
+  display: ${({ display }) => (display ? "block" : "none")};
+  left: 50%;
+  top: 1%;
+  cursor: pointer;
+  &:hover {
+    color: #1aaffc;
+  }
+`;
+
 export default function CheatSheet() {
+  const [info, setInfo] = useState(null);
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    setInfo(`Copied the text to the clipboard`);
+  };
+
   return (
     <Content>
+      <StyledInfo display={info !== null} onClick={() => setInfo(null)}>
+        <Tile>{info}</Tile>
+      </StyledInfo>
       <StyledRow>
         <Tile>
           <h1>Stock</h1>
@@ -30,26 +61,33 @@ export default function CheatSheet() {
       </StyledRow>
       <StyledH2>Copier</StyledH2>
       <StyledRow>
-        <Tile padding="10px">
+        <Tile padding="12px">
           <div>
-            <Tile padding="10px">
-              <p>Pl</p>
+            <Tile padding="12px">
+              <StyledCopy onClick={() => copyToClipboard(pl)}>Pl</StyledCopy>
             </Tile>
-            <Tile padding="10px">
-              <p>En</p>
+            <Tile padding="12px">
+              <StyledCopy onClick={() => copyToClipboard(en)}>En</StyledCopy>
             </Tile>
           </div>
           <div>
-            <Tile padding="10px">
-              <p>Link</p>
+            <Tile padding="12px">
+              <StyledCopy onClick={() => copyToClipboard(link)}>
+                Link
+              </StyledCopy>
             </Tile>
-            <Tile padding="10px">
-              <p>Git</p>
+            <Tile padding="12px">
+              <StyledCopy onClick={() => copyToClipboard(git)}>Git</StyledCopy>
             </Tile>
           </div>
         </Tile>
         <Tile>
-          <h1>CV</h1>
+          <Link
+            href="https://github.com/ZTKpro/ds-code.netlify.app/blob/main/components/assets/Damian_Szmurło_CV.pdf"
+            target="_blank"
+          >
+            <StyledCopy>CV</StyledCopy>
+          </Link>
         </Tile>
       </StyledRow>
     </Content>
