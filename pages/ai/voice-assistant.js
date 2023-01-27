ac902cb6001e4c3255f5a66af27427225132bdad
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2023-01-27 20:02:50 +0100

    VoiceAssistant - ready to create recognition speach

--- pages/ai/voice-assistant.js
+++ pages/ai/voice-assistant.js
@@ -5,14 +5,15 @@ import styled from "styled-components";
 import Content from "../../components/content";
 import Bubble from "../../components/bubble";
 
-const StyledInfo = styled.h4`
+const StyledInfo = styled.p`
   position: absolute;
   top: 60%;
-  left: 52.5%;
+  left: 51%;
+  font-size: 28px;
 `;
 
 export default function VoiceAssistant() {
-  const defaultOpacity = 0.5;
+  const defaultOpacity = 0.7;
 
   const [opacityAssistent, setOpacityAssistent] = useState(defaultOpacity);
   const [info, setInfo] = useState("");
